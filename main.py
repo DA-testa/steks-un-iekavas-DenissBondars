5b855e99f9169b0696e6f6dfafa87815872d9f9d
Deniss Bondars <124886980+DenissBondars@users.noreply.github.com>
2023-02-14 21:50:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,11 +14,21 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
+            opening_brackets_stack.append(Bracket(next, position + 1))
             pass
 
         if next in ")]}":
             # Process closing bracket, write your code here
+            if (len(opening_brackets_stack) == 0) or not(are_matching(opening_brackets_stack[-1].char, next)):
+                print (i+1)
+                return i+1
+            opening_brackets_stack.pop()
             pass
+    if (len(opening_brackets_stack) == 0):
+        print("Success")
+    else:
+        print(opening_brackets_stack[-1].position)
+    pass
 
 
 def main():
