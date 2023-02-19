4c8e9a4e063884c753050c11a30086a62e080aa1
Deniss Bondars <124886980+DenissBondars@users.noreply.github.com>
2023-02-19 04:04:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def find_mismatch(text):
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            if (len(opening_brackets_stack) == 0) or not(are_matching(opening_brackets_stack[-1].char, next)):
+            if not(are_matching(opening_brackets_stack[-1].char, next)):
                 return i+1
             opening_brackets_stack.pop()
             pass
